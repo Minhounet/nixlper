350ca3e6d560e6b45aa18c91bb677251c83240fa
Minhounet <Minhounet@users.noreply.github.com>
2023-12-15 23:57:38 +0100

    Introduce archive creation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
 plugins {
+    id("distribution")
 }
 
 group = "com.qmt"
 version = "1.0-SNAPSHOT"
 
+tasks.distZip {
+    buildContent()
+}
+
+tasks.distTar {
+    buildContent()
+}
+
+fun AbstractArchiveTask.buildContent() {
+    into("/") {
+        from("src/main/bash/nixlper.sh")
+    }
+}
